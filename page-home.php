bc29ffc3db10cf837efc451c1444ba4b3601b815
feliphepaz <feliphe.s.paz@gmail.com>
2021-08-23 12:16:46 -0300

    style do proxima partida feito

--- page-home.php
+++ page-home.php
@@ -1,5 +1,6 @@
 <?php 
 // Template Name: Home
+$style = get_stylesheet_directory_uri();
 
 function post_data($arg) {
     $posts = get_posts()[$arg];
@@ -27,6 +28,34 @@ function post_data($arg) {
                 <legend><?= post_data(1)['date'] ?></legend>
                 <h2><?= post_data(1)['title']; ?></h2>
             </a>
+        </div>
+    </section>
+
+    <section class='match'>
+        <div class='container'>
+            <div class='match-data'>
+                <p class='league'>Campeonato Baiano</p>
+                <p class='stadium'>Estádio Mario Pessoa</p>
+                <div class='versus'>
+                    <img src="<?= $style; ?>/img/barcelona_match.png" alt="">
+                    <span>X</span>
+                    <img src="<?= $style; ?>/img/olimpia_match.png" alt="">
+                </div>
+                <p class='date'>11 de Agosto</p>
+            </div>
+
+            <div class='divide'></div>
+
+            <div class='match-day'>
+                <p class='rest'>Faltam</p>
+                <div class='day'>
+                    <p>04 <span>D</span></p>
+                    <p>13 <span>H</span></p>
+                    <p>06 <span>M</span></p>
+                    <p>20 <span>S</span></p>
+                </div>
+                <a class='cta' href="">Comprar Ingressos</a>
+            </div>
         </div>
     </section>
 <?php get_footer(); ?>
