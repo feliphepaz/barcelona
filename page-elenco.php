068ff92eb420e6908a44a9bba795a44b9b45e96f
feliphepaz <feliphe.s.paz@gmail.com>
2021-09-14 18:15:40 -0300

    site pré finalizado

--- page-elenco.php
+++ page-elenco.php
@@ -12,6 +12,21 @@ $style = get_stylesheet_directory_uri(); ?>
 
 <section class='elenco'>
     <div class='container'>
+			<div class='goleiros'>
+				<h2 class='title'>Goleiros</h2>
+				<ul>
+					<li>
+						<img src="<?= $style ?>/img/memphis.png" alt="">
+						<h3>João Victor</h3>
+						<div class='tooltip-player'>
+							<p>João Victor é um futebolista brasileiro que atua como atacante. Atualmente joga pelo Barcelona e pela Seleção brasileira. É filho de pai português e mãe brasileira.</p>
+						</div>
+					</li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
     </div>
 </section>
 
